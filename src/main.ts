94b944aaa92d739a58e36a95941080e6cd46d929
TuanaCelik <tuanacelik95@gmail.com>
2021-11-09 15:51:03 +0100

    first notebook draft

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
 import "./styles.css";
+import "@spectrum-web-components/theme/sp-theme.js";
+import "@spectrum-web-components/theme/src/themes.js";
+import "@spectrum-web-components/button/sp-button.js";
+import "@spectrum-web-components/button/sp-clear-button.js";
+import "@spectrum-web-components/button-group/sp-button-group.js";
